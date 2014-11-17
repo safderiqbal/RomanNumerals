5b6c02c78e74ddad2b614359334dd678a06daa00
Safder Iqbal <safder.iqbal@gmail.com>
2014-11-17 23:39:09 +0000

    Switched from recursion to use the .net framework

--- NumberToRomanNumeral/RomanNumeralGenerator.cs
+++ NumberToRomanNumeral/RomanNumeralGenerator.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
 
 namespace NumberToRomanNumeral
 {
     /// <summary>
-    /// A simple Roman Numeral generator that uses recursion and string concatenation
+    /// An alternate Roman Numeral generator, that doesn't use recursion and instead leverages features of the .Net 4.0 framework
     /// </summary>
-    class RomanNumeralGenerator: IRomanNumeralGenerator
+    class RomanNumeralGenerator_Alternate : IRomanNumeralGenerator
     {
         public string generate(int number)
         {
@@ -16,34 +19,76 @@ namespace NumberToRomanNumeral
             if (number < 1)
                 return string.Empty;
 
-            if (number >= 1000)
-                return "M" + generate(number - 1000);
-            if (number >= 900)
-                return "CM" + generate(number - 900);
-            if (number >= 500)
-                return "D" + generate(number - 500);
-            if (number >= 400)
-                return "CD" + generate(number - 400);
-            if (number >= 100)
-                return "C" + generate(number - 100);
-            if (number >= 90)
-                return "XC" + generate(number - 90);
-            if (number >= 50)
-                return "L" + generate(number - 50);
-            if (number >= 40)
-                return "XL" + generate(number - 40);
-            if (number >= 10)
-                return "X" + generate(number - 10);
-            if (number >= 9)
-                return "IX" + generate(number - 9);
-            if (number >= 5)
-                return "V" + generate(number - 5);
-            if (number >= 4)
-                return "IV" + generate(number - 4);
-            if (number >= 1)
-                return "I" + generate(number - 1);
-            // All code paths should be accounted for, and this should not be reached
-            throw new ArgumentOutOfRangeException("number");
+            // Setup variables
+            StringBuilder numeralString = new StringBuilder();
+            int remainingValueToConvert = number;
+            Tuple<string, int> numeralValue;
+
+            // Iterate over M as many times as required
+            numeralValue = GenerateNumeral("M", 1000, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            // and repeat for the other unique numerals
+            numeralValue = GenerateNumeral("CM", 900, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("D", 500, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("CD", 400, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("C", 100, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("XC", 90, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("L", 50, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("XL", 40, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("X", 10, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("IX", 9, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("V", 5, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("IV", 4, remainingValueToConvert);
+            remainingValueToConvert = numeralValue.Item2;
+            numeralString.Append(numeralValue.Item1);
+
+            numeralValue = GenerateNumeral("I", 1, remainingValueToConvert);
+            numeralString.Append(numeralValue.Item1);
+
+            return numeralString.ToString();
+        }
+
+        private static Tuple<string, int> GenerateNumeral(string numeral, int numeralValue, int number)
+        {
+            // Return the numeral repeated as many times as possibly (via modding) and return the current value of the number (after division)
+            if (number >= numeralValue)
+            {
+                Debug.WriteLine(numeral + ", worth " + numeralValue + " repeated " + number / numeralValue + " times for value " + number);
+                return Tuple.Create(string.Concat(Enumerable.Repeat(numeral, number / numeralValue)), number % numeralValue);
+            }
+            return Tuple.Create(string.Empty, number);
         }
     }
 }
